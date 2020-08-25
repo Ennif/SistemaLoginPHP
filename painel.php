a57f7046cc039c33041875f3707c2633d3e41acb
ezau <ezaumateus97@gmail.com>
2020-08-24 21:32:06 -0300

    modificacoesMessagemDeErro

--- painel.php
+++ painel.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 
+include('verifica_login.php');
+
 ?>
 
-<h2>Olá<?php echo $_SESSION['usuario'];?></h2>
+<h2>Olá,<?php echo $_SESSION['usuario'];?></h2>
+
+<h2><a href="logout.php">Sair</a></h2 >
+
